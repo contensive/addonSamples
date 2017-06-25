ba2617380650d30f9febcd99da3367c256ade4e0
jay <jay@contensive.com>
2017-06-24 21:16:10 -0400

    consolodating, adding models and script

--- etc/xmultiFormAjaxCs/Properties/AssemblyInfo.cs
+++ etc/xmultiFormAjaxCs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vb2005AddonTemplate")]
+[assembly: AssemblyTitle("managerSample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vb2005AddonTemplate")]
+[assembly: AssemblyProduct("managerSample")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
